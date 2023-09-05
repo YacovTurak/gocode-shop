1082a7daf4e13cbf3bd9d5532dd214516b1ad2b6
hnsywnwtsly <101198854+hnsywnwtsly@users.noreply.github.com>
2023-09-06 01:02:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./src/components/App";
+function App() {
+    <div>aaaa</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+import "./style.css";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Products />
+        </div>
+    );
+}
+
+export default App;

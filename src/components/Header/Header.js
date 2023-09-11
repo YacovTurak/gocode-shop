0fd942f0ec19bb713230adf70f545e2559bf0b65
YacovTurak <0018758@gmail.com>
2023-09-11 23:37:08 +0300

    added filter products

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import "./Header.css";
 
-function Header() {
+function Header({ categories, onFilter }) {
     return (
         <nav className="product-filter">
             <h1>Jackets</h1>
@@ -8,15 +8,17 @@ function Header() {
             <div className="sort">
                 <div className="collection-sort">
                     <label>Filter by:</label>
-                    <select>
-                        <option value="/">All Jackets</option>
-                        <option value="/">2016</option>
-                        <option value="/">jacket</option>
-                        <option value="/">Jackets</option>
-                        <option value="/">layers</option>
-                        <option value="/">Obermeyer</option>
-                        <option value="/">Roxy</option>
-                        <option value="/">womens</option>
+                    <select
+                        onChange={(e) => {
+                            onFilter(e.target.value);
+                        }}
+                    >
+                        <option value="all">all</option>
+                        {categories.map((category) => (
+                            <option key={category} value={category}>
+                                {category}
+                            </option>
+                        ))}
                     </select>
                 </div>
 
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import "./Loader.css";
+export default function Loader() {
+    return (
+        <main className="spinner-examples">
+            <div className="example">
+                <span className="smooth spinner" />
+            </div>
+        </main>
+    );
+}
